4c2ea6e9e540ac82af8e6d61b37572a28a010ef2
Zyrs9 <jesuis.ozbay@gmail.com>
2025-08-05 16:44:26 +0300

    SVG to morse and reverse borrowed and added with a main menu functionality

--- morse_handler.py
+++ morse_handler.py
@@ -1,16 +1,6 @@
 # morse_handler.py
+import morse_dict
 
-MORSE_CODE_DICT = {
-    '.-': 'A', '-...': 'B', '-.-.': 'C', '-..': 'D',
-    '.': 'E', '..-.': 'F', '--.': 'G', '....': 'H',
-    '..': 'I', '.---': 'J', '-.-': 'K', '.-..': 'L',
-    '--': 'M', '-.': 'N', '---': 'O', '.--.': 'P',
-    '--.-': 'Q', '.-.': 'R', '...': 'S', '-': 'T',
-    '..-': 'U', '...-': 'V', '.--': 'W', '-..-': 'X',
-    '-.--': 'Y', '--..': 'Z', '-----': '0', '.----': '1',
-    '..---': '2', '...--': '3', '....-': '4', '.....': '5',
-    '-....': '6', '--...': '7', '---..': '8', '----.': '9'
-}
 
 
 class MorseHandler:
@@ -37,7 +27,7 @@ class MorseHandler:
 
     def _commit_current_symbol(self):
         if self.current_symbol:
-            char = MORSE_CODE_DICT.get(self.current_symbol, '?')
+            char = morse_dict.MORSE_CODE_DICT.get(self.current_symbol, '?')
             self.decoded_text += char
             self.current_symbol = ""
 
